fa6aba202e09cbdee9b3040b7889cea6f189badc
Christofer Fröberg <christofer.froberg@gmail.com>
2023-01-19 21:05:04 +0100

    added search by name

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,9 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> logger;
     private readonly IDataStore dataStore;
-
+    
+    [BindProperty]
+    public string? SearchTerm { get; set; }
     public IEnumerable<Album> Albums { get; set; } = new List<Album>();
 
     public IndexModel(ILogger<IndexModel> logger, IDataStore dataStore)
@@ -14,9 +16,19 @@ public class IndexModel : PageModel
         this.logger = logger;
         this.dataStore = dataStore;
     }
+    public async Task OnPostAsync()
+    {
+        System.Console.WriteLine("here post");
+        System.Console.WriteLine($"The SearchTerm: {SearchTerm}");
+        var albums = await dataStore.GetAlbums();
+        var searchResult = albums.Where(m => m.Title.Contains(SearchTerm ?? ""));
+        this.Albums = searchResult;
+    }
 
     public async Task OnGetAsync()
     {
+        System.Console.WriteLine("here");
+        System.Console.WriteLine($"The SearchTerm: {SearchTerm}");
         this.Albums = await dataStore.GetAlbums();
     }
 }
